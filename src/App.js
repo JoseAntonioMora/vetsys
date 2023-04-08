0ed42ef8a2aced407519e7cbe5815bf8904bb9bb
Jose Antonio Mora <j_antonio_mora@hotmail.com>
2023-04-07 22:37:43 -0600

    Se agrega Login a App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Login } from "./components/Login";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">Sistema Veterinaria</header>
+            <Login />
 		</div>
 	);
 }
